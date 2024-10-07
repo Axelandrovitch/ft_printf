fb733455cba06584dab1868a87891c1e2e135c38
Axelandrovitch <axel.a.hetru@gmail.com>
2024-10-07 20:32:19 +0200

    first commit

---
+++ libftprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahetru <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/05 18:15:35 by ahetru            #+#    #+#             */
+/*   Updated: 2024/10/05 18:15:37 by ahetru           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTF_H
+# define PRINTF_H
+#include <stdarg.h>
+#include "libft/libft.h"
+#include <stdio.h> //for testing purposes
+
+int	ft_printf(const char *fstr, ...);
+
+#endif
